fb0b0516bf37bf32570024c18fefad32e8cfb087
SpringBootHelloWorld <1016449508@qq.com>
2022-08-06 14:19:10 +0800

    外卖

---
+++ src/main/java/com/xyl/mydelivery/service/DishFlavorService.java
@@ -0,0 +1,6 @@
+package com.xyl.mydelivery.service;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xyl.mydelivery.entity.DishFlavor;
+
+public interface DishFlavorService extends IService<DishFlavor> {
+}
---
+++ src/main/java/com/xyl/mydelivery/service/mpl/DishFlavorServiceImpl.java
@@ -0,0 +1,12 @@
+package com.xyl.mydelivery.service.mpl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xyl.mydelivery.entity.DishFlavor;
+import com.xyl.mydelivery.mapper.DishFlavorMapper;
+import com.xyl.mydelivery.service.DishFlavorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor>
+        implements DishFlavorService {
+}
